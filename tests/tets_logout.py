8c05076888a6e80964dd50ddbfb6a90c6cd27ee4
Elizabeth Toujilina <eliztoujilina@yandex.com>
2023-05-29 17:16:56 +0300

    add new commit

---
+++ tests/tets_logout.py
@@ -0,0 +1,29 @@
+from time import sleep
+from Sprint_3.locators import *
+
+
+def test_log_out(browser):
+    browser.get("https://stellarburgers.nomoreparties.site/")
+
+    # Нажатие на кнопку "Войти в аккаунт"
+    sign_in_button = browser.find_element(*MainPageLocators.SIGN_IN_BUTTON)
+    sign_in_button.click()
+
+    # Ввод email и пароля
+    email_input = browser.find_element(*RegistrationPageLocators.EMAIL_INPUT)
+    email_input.send_keys("eliz_toujilina_10_123@ya.ru")
+
+    password_input = browser.find_element(*RegistrationPageLocators.PASSWORD_INPUT)
+    password_input.send_keys("hello111")
+
+    # Нажатие на кнопки "Войти" и "Выйти"
+    login_button = browser.find_element(*LoginPageLocators.LOGIN_BUTTON)
+    login_button.click()
+    sleep(1)
+    account_btn = browser.find_element(*MainPageLocators.ACCOUNT_BUTTON)
+    account_btn.click()
+    sleep(2)
+    logout_btn = browser.find_element(*AccountPageLocators.LOGOUT_BUTTON)
+    logout_btn.click()
+    sleep(2)
+    assert browser.current_url == "https://stellarburgers.nomoreparties.site/login"
